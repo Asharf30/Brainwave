9cd7e8faf0902e3e4a7beaa0e1226640d152f843
Asharf Osama <2300109@dhic.edu.eg>
2026-06-10 15:19:43 +0300

    fix: correct button text and update button styles for better UI consistency

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1 className="text-2xl font-bold">Hello Tailwind v4</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Button className="mt-10 " href="#login">
-          Click mesadsadasdasd
+          Click 
         </Button>
       </div>
       <ButtonGradient />
--- src/Copmonents/Button.jsx
+++ src/Copmonents/Button.jsx
@@ -1,9 +1,12 @@
 import ButotonSvg from "../assets/svg/ButtonSvg";
 
 const Button = ({ className, href, children, OnClick, px, white }) => {
-  // const classes =()=>()
+  const classes = `button relative iniline-flex items-center justify-center h-11 
+  transition-colors hover:text-color-6
+  
+  `;
   const renderButton = () => (
-    <button className=" cursor-pointer">
+    <button className={classes}>
       <span>{children}</span>
       {ButotonSvg(white)}
     </button>
